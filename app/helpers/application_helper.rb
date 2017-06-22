8f79be10bf2c0dc0344676c6f11534ed42257e2a
Neil Johari <neil@johari.tech>
2017-06-22 14:45:00 -0400

    Add logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Rails
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
+  end
 end
