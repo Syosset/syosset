74f688d4d4e2f36f0fc1951659766fc112f99fee
Neil Johari <neil@johari.tech>
2017-06-23 15:46:09 -0400

    Set up factory_girl, wipe db before each test

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 
   gem 'rspec-rails', '~> 3.5'
+  gem 'factory_girl'
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,11 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # Clean/Reset Mongoid DB prior to running each test.
+  config.before(:each) do
+    Mongoid::Sessions.default.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
