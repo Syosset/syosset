a364fab01ef9dcf8ed67955c657e440a3df585ec
Neil Johari <neil@johari.tech>
2017-06-27 18:00:53 -0400

    Disable forms for changing user info, only allow Google Oauth from syosset.k12.ny.us

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,5 +275,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {hd: "syosset.k12.ny.us"}
 end
