4edac373cc97d907139fd31d0797dd6d544441ab
Neil Johari <neil@johari.tech>
2017-06-28 11:37:09 -0400

    Add action to make new announcement in announceable announcement index

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,21 +4,23 @@ class AnnouncementsController < ApplicationController
   before_action :get_announceable, only: [:index]
 
   def index
+    actions_builder = ActionsBuilder.new(current_holder)
     @announcements = if @announceable
+      actions_builder.require(:edit, @announceable).add_action("New Announcement", :get, new_admin_announcement_path("#{@announceable.class.to_s.downcase}_id" => @announceable.id))
       @announceable.announcements
     else
       Announcement.all
     end
 
-    @actions = [] # TODO
+    @actions = actions_builder.actions
   end
 
   def show
     actions_builder = ActionsBuilder.new(current_holder)
     @announceable = @announcement.announceable
-    actions_builder.require(:edit, @announcement.announceable).add_action("Edit Announcement", :get, edit_admin_announcement_path(@announcement, "#{@announcement.announceable.class.to_s.downcase}_id" => @announcement.announceable.id))
-    actions_builder.require(:edit, @announcement.announceable).add_action("Destroy Announcement", :delete, admin_announcement_path(@announcement), data: { confirm: 'Are you sure?' })
-    actions_builder.require(:edit, @announcement.announceable).add_action("Request Frontpage Visibility", :get, new_admin_escalation_request_path(announcement_id: @announcement))
+    actions_builder.require(:edit, @announceable).add_action("Edit Announcement", :get, edit_admin_announcement_path(@announcement, "#{@announceable.class.to_s.downcase}_id" => @announceable.id))
+    actions_builder.require(:edit, @announceable).add_action("Destroy Announcement", :delete, admin_announcement_path(@announcement), data: { confirm: 'Are you sure?' })
+    actions_builder.require(:edit, @announceable).add_action("Request Frontpage Visibility", :get, new_admin_escalation_request_path(announcement_id: @announcement))
     @actions = actions_builder.actions
   end
 
