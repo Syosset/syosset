2deab54c5e737ccc887a1f43a45ffbfe46965710
Kailan Blanks <kailan@kmp.pw>
2017-06-28 21:47:24 +0100

    Add caching to static pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,10 @@ class WelcomeController < ApplicationController
   def index
     @announcements = (Announcement.escalated(8).sort_by!(&:created_at) + Announcement.desc(:created_at).limit(8).to_a).first(8).uniq
     # First 8 escalated announcements. If there aren't 8, we'll pad with the latest announcements and hopefully that'll make up for it.
+    expires_in 5.minutes, :public => true
   end
 
   def about
+    expires_in 5.minutes, :public => true
   end
 end
