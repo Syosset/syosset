0ca1537a3ec6bb233ea2b1f049851e8eb4db2a42
Neil Johari <neil@johari.tech>
2017-07-04 13:27:25 -0400

    Make links use collaborator system, add x-mark icon for link deletion

--- config/initializers/scram.rb
+++ config/initializers/scram.rb
@@ -25,8 +25,9 @@ module Scram
     return policy
   }
 
-  DEFAULT_POLICIES << collaborator_policy.call(Department)
-  DEFAULT_POLICIES << collaborator_policy.call(Announcement)
+  DEFAULT_POLICIES << collaborator_policy.(Department)
+  DEFAULT_POLICIES << collaborator_policy.(Announcement)
+  DEFAULT_POLICIES << collaborator_policy.(Link)
 
   DEFAULT_POLICIES.freeze
 
