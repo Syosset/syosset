7059e87ca41f351b55815f9ace2ca3cc0a5fd75b
Neil Johari <neil@johari.tech>
2017-07-04 14:53:44 -0400

    Display first 3 escalated links onto front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
   def index
     @announcements = (Announcement.escalated(8).sort_by!(&:created_at) + Announcement.desc(:created_at).limit(8).to_a).first(8).uniq
     # First 8 escalated announcements. If there aren't 8, we'll pad with the latest announcements and hopefully that'll make up for it.
+
+    @links = Link.escalated(3).sort_by!(&:created_at)
+
     expires_in 5.minutes, :public => true
   end
 
