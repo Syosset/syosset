5383502aa83b58805327d2fd9811b701ea890b0c
Neil Johari <neil@johari.tech>
2017-08-25 16:04:30 -0400

    Move day indicator on mobile to be neater

--- app/assets/javascripts/dynamic_schedule.js
+++ app/assets/javascripts/dynamic_schedule.js
@@ -3,13 +3,15 @@ function valBetween(v, min, max) {
 };
 
 $(document).on('turbolinks:load', function() {
-  $("#day").on('inserted.bs.popover', function () {
-    var curTime = new Date();
-    var curSeconds = (curTime.getHours() * 60 * 60) + (curTime.getMinutes() * 60) + curTime.getSeconds();
-    //curSeconds = 33120;
-    curSeconds = valBetween(curSeconds, 27540, 51660);
-    var percentOfDay = (curSeconds-27540) / (51660-27540);
-    var topLoc = Math.floor($(".schedule").height() * percentOfDay);
-    $("#movable").css("top", topLoc + "px");
-  });
+  $(".day").each((function() {
+    $(this).on('inserted.bs.popover', function () {
+      var curTime = new Date();
+      var curSeconds = (curTime.getHours() * 60 * 60) + (curTime.getMinutes() * 60) + curTime.getSeconds();
+      curSeconds = 33120;
+      curSeconds = valBetween(curSeconds, 27540, 51660);
+      var percentOfDay = (curSeconds-27540) / (51660-27540);
+      var topLoc = Math.floor($(".schedule").height() * percentOfDay);
+      $("#movable").css("top", topLoc + "px");
+    });
+  }));
 });
