2b64848d0420ea430ace2a6dd8b95eafce298512
Neil Johari <neil@johari.tech>
2017-08-25 16:47:25 -0400

    Remove debug code from dynamic schedule

--- app/assets/javascripts/dynamic_schedule.js
+++ app/assets/javascripts/dynamic_schedule.js
@@ -7,8 +7,10 @@ $(document).on('turbolinks:load', function() {
     $(this).on('inserted.bs.popover', function () {
       var curTime = new Date();
       var curSeconds = (curTime.getHours() * 60 * 60) + (curTime.getMinutes() * 60) + curTime.getSeconds();
-      curSeconds = 33120;
       curSeconds = valBetween(curSeconds, 27540, 51660);
+      if(curSeconds == 27540 || curSeconds == 51660) {
+        $("#movable").hide();
+      }
       var percentOfDay = (curSeconds-27540) / (51660-27540);
       var topLoc = Math.floor($(".schedule").height() * percentOfDay);
       $("#movable").css("top", topLoc + "px");
