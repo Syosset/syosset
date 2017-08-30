81c3fe9cd7c86a4825a1a29b4e2c74cd563ec5e8
Kailan Blanks <kailan@kmp.pw>
2017-08-30 02:50:41 +0100

    Rename app from "Web" to "Syosset" (#44)

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -6,7 +6,7 @@ module Peek::Views
   end
 end
 
-Peek.into Peek::Views::Git, nwo: "Syosset/Web", sha: ENV["GIT_REV"]
+Peek.into Peek::Views::Git, nwo: "Syosset/syosset", sha: ENV["GIT_REV"]
 Peek.into Peek::Views::Admin
 Peek.into Peek::Views::Host, host: ENV["HOSTNAME"]
 Peek.into Peek::Views::Redis
