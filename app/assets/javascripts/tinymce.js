e9e3b75610534bdd9983f8034f0fcf7e495ef4cc
Neil Johari <neil@johari.tech>
2017-08-31 18:04:18 -0400

    Prevent tinymce from using relative URLs

--- app/assets/javascripts/tinymce.js
+++ app/assets/javascripts/tinymce.js
@@ -3,6 +3,9 @@ function init_tiny() {
     selector: '.editable-content',
     inline: true,
     plugins: "table insertdatetime preview link image media searchreplace contextmenu paste directionality fullscreen noneditable visualchars nonbreaking template save",
+    relative_urls: false,
+    remove_script_host: false,
+    convert_urls: true,
     toolbar: [
     "save | undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
     "print preview   | fontselect fontsizeselect | forecolor backcolor emoticons | codesample"],
