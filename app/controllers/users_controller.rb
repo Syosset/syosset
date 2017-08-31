96a596443d54856a91f892651acd40c0edd9b6a4
Neil Johari <neil@johari.tech>
2017-08-30 21:08:48 -0400

    Polish up user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_path, alert: "Only staff members can have profiles." unless @user.staff?
+    @periods = @user.periods.asc(:period)
   end
 end
