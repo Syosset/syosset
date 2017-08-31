e12de586a1cd93e0c8f3d52d27c4cfbad8cb2bec
Neil Johari <neil@johari.tech>
2017-08-30 21:16:38 -0400

    Allow course selection for schedules

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -2,6 +2,7 @@ class PeriodsController < ApplicationController
 
   before_action :check_staff, except: [:index]
   before_action :get_period, only: [:edit, :update]
+  before_action :get_user_courses, only: [:new, :edit]
 
   def index
     @user = User.find(params[:user_id])
@@ -52,5 +53,10 @@ class PeriodsController < ApplicationController
     def get_period
       @period = Period.find(params[:id])
     end
+
+    def get_user_courses
+      @courses = CollaboratorGroup.with_member(current_user).select {|x| x.collaboratable.is_a? Course }.map(&:collaboratable)
+    end
+
 
 end
