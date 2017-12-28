850c7f6252b9c6c9b17ad3adc0954e5d60227149
Kailan Blanks <kailan@kmp.pw>
2017-12-28 01:00:44 +0000

    Remove christmas banner :(

--- app/assets/javascripts/christmas.js
+++
@@ -1,8 +0,0 @@
-//= require parallax-js/dist/parallax
-
-document.addEventListener("turbolinks:load", function(event) {
-  var scene = document.getElementById('christmasParallax');
-  if (scene) {
-    var parallax = new Parallax(scene);
-  }
-});
