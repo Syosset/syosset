bf8754a6dd8dcf09fbff5c295bbbe309c7bae4f3
Kailan Blanks <kailan@kmp.pw>
2018-01-04 00:00:40 +0000

    Increase GA site speed sample rate to 5%

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-105323459-1', 'auto');
+ga('create', 'UA-105323459-1', 'auto', {'siteSpeedSampleRate': 5});
 document.addEventListener("turbolinks:load", function(event) {
   ga("set", "location", event.data.url);
   ga("send", "pageview");
