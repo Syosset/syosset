f12ecccce2f9a9b3edee055b0879dca0a4dc97e4
Neil Johari <neil@johari.tech>
2018-01-13 10:50:41 -0500

    Remove implicit modifier detection

--- app/models/history_tracker.rb
+++ app/models/history_tracker.rb
@@ -1,11 +1,3 @@
 class HistoryTracker
   include Mongoid::History::Tracker
-
-  before_save :set_modifier
-  before_update :set_modifier
-
-  protected
-  def set_modifier
-    self.modifier = Current.user
-  end
 end
