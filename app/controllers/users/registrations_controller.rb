28365b2d59f545a5f5b1630ee1f876eaa4532433
Kailan Blanks <kailan@kmp.pw>
2018-01-23 18:45:56 +0000

    Show validation errors on user edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update
     authorize current_user
-    current_user.update!(params.require(:user).permit(:bio, :picture))
-    redirect_to user_path(current_user)
+    if current_user.update(params.require(:user).permit(:bio, :picture))
+      redirect_to user_path(current_user)
+    else
+      render action: 'edit'
+    end
   end
 
 end
