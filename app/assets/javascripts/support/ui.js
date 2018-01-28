5d6e0a0b2f997eaae5d3a1d7fc7c36d3ae01c34e
Neil Johari <neil@johari.tech>
2018-01-28 14:59:04 -0500

    Add basic styling for chat

--- app/assets/javascripts/support/ui.js
+++ app/assets/javascripts/support/ui.js
@@ -3,7 +3,7 @@ var loaded = false;
 function loadChat() {
   if(loaded) return;
   loaded = true;
-  
+
   $("#support_ui").hide();
   $("#open_support").click(function() {
     Support.openThread(function(thread) {
@@ -15,14 +15,15 @@ function loadChat() {
       });
 
       thread.onMessage(function(msg){
-        $('.messages').append('<div class=\'message\'>' + msg.message + '</div>');
+        $('.messages').append('<div class=\'message\'>' + msg.sender.name + ': ' + msg.message + '</div>');
       });
     });
   });
-
+  $("#dismiss_support").click(function() {
+    $("#support_ui").hide();
+    $("#open_support").show();
+  });
 }
-
-
 
 $(document).ready(loadChat);
 $(document).on('turbolinks:load', loadChat);
