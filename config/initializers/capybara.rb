a84048c49d16c8748a0b8623f57571f292395a78
Kailan Blanks <kailan@kmp.pw>
2018-02-10 20:07:50 +0000

    Only load capybara in test environment

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,8 +1,10 @@
-require 'capybara/poltergeist'
+if Rails.env.test?
+  require 'capybara/poltergeist'
 
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false)
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, js_errors: false)
+  end
+
+  # Configure Capybara to use Poltergeist as the driver
+  Capybara.default_driver = :poltergeist
 end
-
-# Configure Capybara to use Poltergeist as the driver
-Capybara.default_driver = :poltergeist
