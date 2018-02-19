750c627ab63a97ce3a5815f068b79385a7555ab5
Neil Johari <neil@johari.tech>
2018-02-19 17:09:37 -0500

    Add modified version of IconHelper from font-awesome-rails

---
+++ app/helpers/icon_helper.rb
@@ -0,0 +1,47 @@
+# Modified version of font-awesome-rails icon helper (adds "aria-hidden" => true automatically)
+module IconHelper
+  def fa_icon(names = 'flag', original_options = {})
+    options = original_options.deep_dup
+    classes = ['fa']
+    classes.concat Private.icon_names(names)
+    classes.concat Array(options.delete(:class))
+    text = options.delete(:text)
+    right_icon = options.delete(:right)
+    icon = content_tag(:i, nil, options.merge(class: classes, "aria-hidden" => true))
+    Private.icon_join(icon, text, right_icon)
+  end
+
+  def fa_stacked_icon(names = 'flag', original_options = {})
+    options = original_options.deep_dup
+    classes = Private.icon_names('stack').concat(Array(options.delete(:class)))
+    base_names = Private.array_value(options.delete(:base) || 'square-o').push('stack-2x')
+    names = Private.array_value(names).push('stack-1x')
+    base = fa_icon(base_names, options.delete(:base_options) || {})
+    icon = fa_icon(names, options.delete(:icon_options) || {})
+    icons = [base, icon]
+    icons.reverse! if options.delete(:reverse)
+    text = options.delete(:text)
+    right_icon = options.delete(:right)
+    stacked_icon = content_tag(:span, safe_join(icons), options.merge(class: classes, "aria-hidden" => true))
+    Private.icon_join(stacked_icon, text, right_icon)
+  end
+
+  module Private
+    extend ActionView::Helpers::OutputSafetyHelper
+
+    def self.icon_join(icon, text, reverse_order = false)
+      return icon if text.blank?
+      elements = [icon, ERB::Util.html_escape(text)]
+      elements.reverse! if reverse_order
+      safe_join(elements, ' ')
+    end
+
+    def self.icon_names(names = [])
+      array_value(names).map { |n| "fa-#{n}" }
+    end
+
+    def self.array_value(value = [])
+      value.is_a?(Array) ? value : value.to_s.split(/\s+/)
+    end
+  end
+end
