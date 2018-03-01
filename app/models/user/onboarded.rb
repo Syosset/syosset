4fcfd5d96acd78501e6d0d3650b44c4e6174c514
Kailan Blanks <kailan@kmp.pw>
2018-03-01 23:38:10 +0000

    Fix onboarding check for profile picture

--- app/models/user/onboarded.rb
+++ app/models/user/onboarded.rb
@@ -2,7 +2,7 @@ module User::Onboarded
   def onboarding_steps
     required = []
     if staff?
-      required << :picture unless picture.blank?
+      required << :picture if picture.blank?
       required << :bio if bio.blank?
     end
     required
