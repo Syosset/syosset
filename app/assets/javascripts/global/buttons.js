08c2e1b613ae64e3ff285501de2321c1ae9f2fd8
Kailan Blanks <kailan@kmp.pw>
2018-10-09 00:51:58 +0100

    Migrate header, navbar and footer to Bootstrap 4

--- app/assets/javascripts/global/buttons.js
+++ app/assets/javascripts/global/buttons.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
   $('.btn-loader').click(function() {
-    $(this).find('.fa').removeClass().addClass('fa fa-spin fa-refresh');
+    $(this).find('.fa, .fab').removeClass().addClass('fa fa-spin fa-spinner');
   });
 });
